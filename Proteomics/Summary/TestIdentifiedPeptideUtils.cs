174ec06761dcdc4a19249742f7f9752df6a3020e
Quanhu Sheng <quanhu.sheng@vanderbilt.edu>
2015-03-25 14:58:09 -0500

    v3.9.2

---
+++ Proteomics/Summary/TestIdentifiedPeptideUtils.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace RCPA.Proteomics.Summary
+{
+  [TestFixture]
+  public class TestIdentifiedPeptideUtils
+  {
+    [Test]
+    public void TestUniqueCount()
+    {
+      var protein = new IdentifiedProtein();
+      var spectrum = new IdentifiedSpectrum();
+
+      var peptides = new List<IIdentifiedPeptide>();
+      peptides.Add(new IdentifiedPeptide(spectrum){
+        Sequence = "ILLLAR"
+      });
+
+      peptides.Add(new IdentifiedPeptide(spectrum){
+        Sequence = "LILIAR"
+      });
+
+      Assert.AreEqual(1, IdentifiedPeptideUtils.GetUniquePeptideCount(peptides));
+
+      peptides.Add(new IdentifiedPeptide(new IdentifiedSpectrum())
+      {
+        Sequence = "LIIIAR"
+      });
+
+      Assert.AreEqual(1, IdentifiedPeptideUtils.GetUniquePeptideCount(peptides));
+    }
+  }
+}
---
+++ Proteomics/Summary/TestIdentifiedProteinGroupBuilder.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace RCPA.Proteomics.Summary
+{
+  [TestFixture]
+  public class TestIdentifiedProteinGroupBuilder
+  {
+    [Test]
+    public void TestBuild()
+    {
+      var pep1 = new IdentifiedPeptide(new IdentifiedSpectrum(new SequestFilename("A", 1, 1, 1, ".dta"))) { Sequence = "A" };
+      var pep2 = new IdentifiedPeptide(new IdentifiedSpectrum(new SequestFilename("B", 1, 1, 1, ".dta"))) { Sequence = "B" };
+      var pep3 = new IdentifiedPeptide(new IdentifiedSpectrum(new SequestFilename("C", 1, 1, 1, ".dta"))) { Sequence = "C" };
+      var pep4 = new IdentifiedPeptide(new IdentifiedSpectrum(new SequestFilename("D", 1, 1, 1, ".dta"))) { Sequence = "D" };
+      var pep5 = new IdentifiedPeptide(new IdentifiedSpectrum(new SequestFilename("E", 1, 1, 1, ".dta"))) { Sequence = "E" };
+      var pep6 = new IdentifiedPeptide(new IdentifiedSpectrum(new SequestFilename("F", 1, 1, 1, ".dta"))) { Sequence = "F" };
+
+      var protein1 = new IdentifiedProtein()
+      {
+        Peptides = new IIdentifiedPeptide[] { pep1, pep3, pep5, pep6 }.ToList()
+      };
+
+      var protein2 = new IdentifiedProtein()
+      {
+        Peptides = new IIdentifiedPeptide[] { pep2, pep3, pep4 }.ToList()
+      };
+
+      //should be removed from final result since all peptides has been included in protein1 and protein2, even one protein contains both peptides
+      var protein3 = new IdentifiedProtein()
+      {
+        Peptides = new IIdentifiedPeptide[] { pep1, pep2 }.ToList()
+      };
+
+      //should be removed from final result since all peptides has been included in protein1
+      var protein4 = new IdentifiedProtein()
+      {
+        Peptides = new IIdentifiedPeptide[] { pep1, pep5 }.ToList()
+      };
+
+      var actual = new IdentifiedProteinGroupBuilder().Build(new IIdentifiedProtein[] { protein1, protein2, protein3 }.ToList());
+      Assert.AreEqual(2, actual.Count);
+      Assert.AreSame(protein1, actual[0][0]);
+      Assert.AreSame(protein2, actual[1][0]);
+    }
+  }
+}
