cced6f2a9b5adf020efe545c36efe7572de8aa02
Quanhu Sheng <quanhu.sheng@vanderbilt.edu>
2015-11-11 20:47:58 -0600

    v4.0.8

---
+++ Proteomics/Comet/TestCometXmlParser.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using RCPA.Proteomics.Summary;
+
+namespace RCPA.Proteomics.Comet
+{
+  [TestFixture]
+  public class TestCometXmlParser
+  {
+    [Test]
+    public void Test()
+    {
+      var spectra = new CometXmlParser()
+      {
+        TitleParser = TitleParserUtils.FindByName("DTA")
+      }.ReadFromFile(@"../../../data/comet.pep.xml");
+
+      Assert.AreEqual(2, spectra.Count);
+      Assert.AreEqual("R.HFM*LEMK.S", spectra[0].Sequence);
+      Assert.AreEqual(1, spectra[0].Rank);
+      Assert.AreEqual(0.778, spectra[0].Score, 0.001);
+      Assert.AreEqual(1.91E+01, spectra[0].ExpectValue, 0.01);
+      Assert.AreEqual(1077, spectra[0].Query.MatchCount);
+      Assert.AreEqual("Elite_CIDIT_Human.minus10dalton.2.2.2.dta", spectra[0].Query.FileScan.LongFileName);
+    }
+  }
+}
---
+++ Proteomics/Database/TestReversedDatabaseBuilder.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using RCPA.Seq;
+
+namespace RCPA.Proteomics.Database
+{
+  [TestFixture]
+  public class TestReversedDatabaseBuilder
+  {
+    [Test]
+    public void TestGetReversedSequence()
+    {
+      var seq = new Sequence("sp|Q12345 sp protein","ABCDE");
+      var options = new ReversedDatabaseBuilderOptions()
+      {
+        DecoyKey = "REVERSED",
+        DecoyType = DecoyType.Start
+      };
+
+      var builder = new ReversedDatabaseBuilder(options);
+      var actual = builder.GetReversedSequence(1, seq);
+
+      Assert.AreEqual("sp|REVERSED_Q12345", actual.Name);
+    }
+  }
+}
