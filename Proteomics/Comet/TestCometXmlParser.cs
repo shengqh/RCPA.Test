73b453e769dde291f0a37326927daaaee0294e13
Quanhu Sheng <quanhu.sheng@vanderbilt.edu>
2015-11-24 08:24:51 -0600

    v4.0.10

--- Proteomics/Comet/TestCometXmlParser.cs
+++ Proteomics/Comet/TestCometXmlParser.cs
@@ -23,7 +23,16 @@ namespace RCPA.Proteomics.Comet
       Assert.AreEqual("R.HFM*LEMK.S", spectra[0].Sequence);
       Assert.AreEqual(1, spectra[0].Rank);
       Assert.AreEqual(0.778, spectra[0].Score, 0.001);
+      Assert.AreEqual(0.001, spectra[0].DeltaScore, 0.001);
+      Assert.AreEqual(80.2, spectra[0].SpScore, 0.01);
+      Assert.AreEqual(7, spectra[0].SpRank);
       Assert.AreEqual(1.91E+01, spectra[0].ExpectValue, 0.01);
+      Assert.AreEqual("sp|Q9BV73|CP250_HUMAN", spectra[0].Proteins[0]);
+      Assert.AreEqual(4, spectra[0].MatchedIonCount);
+      Assert.AreEqual(10, spectra[0].TheoreticalIonCount);
+      Assert.AreEqual(803.399995, spectra[0].TheoreticalMass, 0.000001);
+      Assert.AreEqual(2, spectra[0].Query.FileScan.Charge);
+
       Assert.AreEqual(1077, spectra[0].Query.MatchCount);
       Assert.AreEqual("Elite_CIDIT_Human.minus10dalton.2.2.2.dta", spectra[0].Query.FileScan.LongFileName);
     }
--- Proteomics/Comet/TestCometXmlRank2Parser.cs
+++ Proteomics/Comet/TestCometXmlRank2Parser.cs
@@ -22,8 +22,13 @@ namespace RCPA.Proteomics.Comet
       Assert.AreEqual(2, spectra.Count);
       Assert.AreEqual("K.M*REGPAK.N", spectra[0].Sequence);
       Assert.AreEqual(3, spectra[0].Rank);
-      Assert.AreEqual("R.HFLEMK.S", spectra[1].Sequence);
-      Assert.AreEqual(2, spectra[1].Rank);
+      Assert.AreEqual(0.743, spectra[0].Score, 0.001);
+      Assert.AreEqual(0.035, spectra[0].DeltaScore, 0.001);
+      Assert.AreEqual(66.0, spectra[0].SpScore, 0.01);
+      Assert.AreEqual(13, spectra[0].SpRank);
+      Assert.AreEqual(2.47E+01, spectra[0].ExpectValue, 0.01);
+      Assert.AreEqual(1077, spectra[0].Query.MatchCount);
+      Assert.AreEqual("Elite_CIDIT_Human.minus10dalton.2.2.2.dta", spectra[0].Query.FileScan.LongFileName);
     }
   }
 }
